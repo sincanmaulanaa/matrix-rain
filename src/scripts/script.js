0c3ace74a46dbf8c769bb162ab6275984d9578d1
Sincan Maulana <sincanmaulanaa@gmail.com>
2022-12-18 13:45:29 +0800

    refactor from ES5 to ES6+

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
 // Initialising the canvas
-var canvas = document.querySelector('canvas'),
+const canvas = document.querySelector('canvas'),
   ctx = canvas.getContext('2d');
 
 // Setting the width and height of the canvas
@@ -7,26 +7,26 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Setting up the letters
-var letters =
+let letters =
   'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
 letters = letters.split('');
 
 // Setting up the columns
-var fontSize = 10,
+const fontSize = 10,
   columns = canvas.width / fontSize;
 
 // Setting up the drops
-var drops = [];
-for (var i = 0; i < columns; i++) {
+const drops = [];
+for (let i = 0; i < columns; i++) {
   drops[i] = 1;
 }
 
 // Setting up the draw function
-function draw() {
+const draw = () => {
   ctx.fillStyle = 'rgba(0, 0, 0, .1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
+  for (let i = 0; i < drops.length; i++) {
+    let text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillStyle = 'rgb(255, 0, 0)';
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
@@ -34,7 +34,7 @@ function draw() {
       drops[i] = 0;
     }
   }
-}
+};
 
 // Loop the animation
 setInterval(draw, 33);
